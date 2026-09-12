94aebc6d995ff99af0474138668286b58c5f24cd
Immmii <leesa7118@gmail.com>
2026-09-12 12:41:44 +0900

    Feat: 딥링크로 앱 화면을 띄우는 mobile 모듈

--- src/modules/index.js
+++ src/modules/index.js
@@ -2,6 +2,7 @@ import * as swagger from "./swagger/index.js";
 import * as auth from "./auth/index.js";
 import * as report from "./report/index.js";
 import * as metrics from "./metrics/index.js";
+import * as mobile from "./mobile/index.js";
 
 // 모듈을 추가하려면 여기 한 줄만 넣으면 된다.
-export const modules = [swagger, auth, report, metrics];
+export const modules = [swagger, auth, report, metrics, mobile];
